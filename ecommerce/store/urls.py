96a9287540662a35856b76e402b137308f03368a
syedhaideralizaidi <haider.zaidiy@gmail.com>
2023-07-11 15:26:34 +0500

    Combined Functions for one URL

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path("cart/", views.CartView.as_view(), name="cart"),
     path("checkout/", views.CheckoutView.as_view(), name="checkout"),
     path("update_item/", views.UpdateItem.as_view(), name="update_item"),
-    path("create_product/", views.ProductCreateView.as_view(), name="create_product"),
     path(
         "create_customer/", views.CustomerCreateView.as_view(), name="create_customer"
     ),
+    path("create_product/", views.ProductCreateView.as_view(), name="create_product"),
     path("product_list/", views.ProductListView.as_view(), name="product_list"),
     path(
         "product/<str:name>/update/",
@@ -22,4 +22,36 @@ urlpatterns = [
         name="customer_delete",
     ),
     path("template/", views.TemplateHomeView.as_view(), name="template_view"),
+    path("order_archive/", views.OrderArchiveView.as_view(), name="order_archive"),
+    path(
+        "product_detail/<int:pk>/",
+        views.ProductDetailView.as_view(),
+        name="product_detail",
+    ),
+    path(
+        "orderitem_create/", views.OrderItemCreateView.as_view(), name="product_detail"
+    ),
+    path(
+        "dayarchive/<int:year>/",
+        views.OrderDayArchiveView.as_view(),
+        name="order_archive_day",
+    ),
+    path(
+        "customer/<str:name>/update/",
+        views.CustomerUpdateView.as_view(),
+        name="customer_update",
+    ),
+    path("createcustomerFBW/", views.customer_create_view, name="customercreate_FBW"),
+    path(
+        "deletecustomerFBW/<str:name>/",
+        views.customer_delete_view,
+        name="deletecreate_FBW",
+    ),
+    path(
+        "updatecustomerFBW/<str:name>/",
+        views.customer_update_view,
+        name="customerupdate_FBW",
+    ),
+    path("customer/<str:name>", views.customer_view, name="customer"),
+    path("customer/", views.customer_view, name="customer"),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.forms import ModelForm
 from django.http import JsonResponse
 from django.conf import settings
 from django.views import View
 import json
 import os
-from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 from django.views.generic import (
     CreateView,
     UpdateView,
@@ -12,13 +14,108 @@ from django.views.generic import (
     ListView,
     TemplateView,
     RedirectView,
+    ArchiveIndexView,
+    YearArchiveView,
+    DetailView,
+    FormView,
+    GenericViewError,
 )
 
-# Create your views here.
 
-from .models import *
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["name", "email"]
 
 
+# Function Based Views
+
+
+@login_required
+def customer_create_view(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.created_by = request.user
+            customer.save()
+            return redirect("/cart")
+    else:
+        form = CustomerForm()
+    return render(
+        request,
+        os.path.join(settings.BASE_DIR, "templates/store/create_customer_FBW.html"),
+        {"form": form},
+    )
+
+
+@login_required
+def customer_delete_view(request, name):
+    customer = get_object_or_404(Customer, name=name)
+    if request.method == "POST":
+        customer.delete()
+        return redirect("/cart")
+    return render(
+        request,
+        os.path.join(settings.BASE_DIR, "templates/store/delete_customer_FBW.html"),
+        {"customer": customer},
+    )
+
+
+@login_required
+def customer_update_view(request, name):
+    customer = get_object_or_404(Customer, name=name)
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = CustomerForm()
+    return render(
+        request,
+        os.path.join(settings.BASE_DIR, "templates/store/update_customer_FBW.html"),
+        {"customer": customer, "form": form},
+    )
+
+
+def customer_view(request, customer_id=None, customer_name=None):
+    if customer_name is not None:
+        customer = get_object_or_404(Customer, pk=customer_id)
+    elif customer_name is not None:
+        customer = get_object_or_404(Customer, name=customer_name)
+    else:
+        customer = None
+    # if customer_name is None :
+    #     customer = Customer()
+
+    if request.method == "POST":
+        if customer is not None:
+            form = CustomerForm(request.POST, instance=customer)
+        else:
+            form = CustomerForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("cart")
+    else:
+        if customer is not None:
+            form = CustomerForm(instance=customer)
+        else:
+            form = CustomerForm()
+
+    context = {
+        "customer": customer,
+        "form": form,
+    }
+    return render(
+        request,
+        os.path.join(settings.BASE_DIR, "templates/store/customer_FBW.html"),
+        context,
+    )
+
+
+# Class Based Views
 class StoreView(View):
     def get(self, request):
         if request.user.is_authenticated:
@@ -88,19 +185,6 @@ class CheckoutView(View):
         )
 
 
-class ProductCreateView(CreateView):
-    model = Product
-    fields = ["name", "price", "digital", "image"]
-    template_name = os.path.join(
-        settings.BASE_DIR, "templates/store/product_create.html"
-    )
-    success_url = ""
-
-    def form_valid(self, form):
-        form.instance.created_by = self.request.user
-        return super().form_valid(form)
-
-
 class CustomerCreateView(CreateView):
     model = Customer
     fields = ["name", "email"]
@@ -114,7 +198,45 @@ class CustomerCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ProductListView(ListView):
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/customer_delete.html"
+    )
+    success_url = "/cart"
+
+    def get_object(self, queryset=None):
+        name = self.kwargs["name"]
+        return Customer.objects.get(name=name)
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    fields = ["name", "email"]
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/update_customer.html"
+    )
+    success_url = "/"
+
+    def get_object(self, queryset=None):
+        name = self.kwargs["name"]
+        return Customer.objects.get(name=name)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ["name", "price", "digital", "image"]
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/product_create.html"
+    )
+    success_url = ""
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class ProductListView(GenericViewError, ListView):
     model = Product
     template_name = os.path.join(settings.BASE_DIR, "templates/store/product_list.html")
     context_object_name = "products"
@@ -133,18 +255,6 @@ class ProductUpdateView(UpdateView):
         return Product.objects.get(name=name)
 
 
-class CustomerDeleteView(DeleteView):
-    model = Customer
-    template_name = os.path.join(
-        settings.BASE_DIR, "templates/store/customer_delete.html"
-    )
-    success_url = "/cart"
-
-    def get_object(self, queryset=None):
-        name = self.kwargs["name"]
-        return Customer.objects.get(name=name)
-
-
 class TemplateHomeView(TemplateView):
     template_name = os.path.join(
         settings.BASE_DIR, "templates/store/home_template.html"
@@ -153,6 +263,16 @@ class TemplateHomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class OrderArchiveView(ArchiveIndexView):
+    model = Order
+    date_field = "date_ordered"
+    allow_empty = True
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/order_archive.html"
+    )
+    paginate_by = 10
 
 
 class UpdateItem(RedirectView):
@@ -183,3 +303,37 @@ class UpdateItem(RedirectView):
             orderItem.delete()
 
         return JsonResponse("Item was added", safe=False)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/product_detail.html"
+    )
+    context_object_name = "product"
+
+
+class OrderItemCreateView(FormView):
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/orderitem_create.html"
+    )
+    success_url = "/cart"
+
+    class OrderItemForm(ModelForm):
+        class Meta:
+            model = OrderItem
+            fields = ["quantity", "order"]
+
+    form_class = OrderItemForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class OrderDayArchiveView(YearArchiveView):
+    model = Order
+    date_field = "date_ordered"
+    template_name = os.path.join(
+        settings.BASE_DIR, "templates/store/orderday_archive.html"
+    )
